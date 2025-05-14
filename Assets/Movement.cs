446e02af26f818e9b0d2dd43a118d4f856172ede
Robin1901 <robin.grafland@gmail.com>
2025-05-14 17:44:47 +0200

    WASD Movement + Sprint + Camera Look

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,37 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
-    public Rigidbody rb;
+    [Header("Speeds")]
+    public float walkSpeed = 5f;
+    public float sprintMultiplier = 1.5f;
 
-    public float moveForeward = 2000f;
-    public float moveBackwards = 2000f;
-    public float moveLeft = 2000f;
-    public float moveRight = 2000f;
+    [Header("Mouse Look")]
+    public float mouseSensitivity = 100f;
+    public Transform cameraTransform;    // char cam
+    private float xRotation = 0f;        // camera pitch
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private CharacterController controller;
+
+    void Start()
     {
-        if (Input.GetKey("w"))
-        {
-            rb.AddForce(0, 0, moveForeward * Time.deltaTime);
-        }
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(0, 0, -moveBackwards * Time.deltaTime);
-        }
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-moveLeft * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(moveRight * Time.deltaTime, 0, 0);
-        }
+        controller = GetComponent<CharacterController>();
 
+        if (cameraTransform == null)
+            cameraTransform = Camera.main.transform;
 
+        Cursor.lockState = CursorLockMode.Locked; // cursor weg
+        Cursor.visible = false;
+    }
 
+    void Update()
+    {
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //cam drehen
+
+        transform.Rotate(Vector3.up * mouseX); //player drehen
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 rawMove = transform.right * x + transform.forward * z; //bewegen in cam richtung
+        if (rawMove.sqrMagnitude > 1f)
+            rawMove = rawMove.normalized; //diagonal nicht schneller
+
+        bool shiftPressed = Input.GetKey(KeyCode.LeftShift);
+
+        float currentSpeed = shiftPressed
+                             ? walkSpeed * sprintMultiplier
+                             : walkSpeed;
+
+        controller.Move(rawMove * currentSpeed * Time.deltaTime); //bewegen
     }
 }
-    
+
+
+
+
