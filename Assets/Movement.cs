b9885993f0455bf36fbbcd775bc46b4639904ec3
Robin1901 <robin.grafland@gmail.com>
2025-05-14 18:36:34 +0200

    FOV Added
    Verschiedene FOVs beim Crouchen, Laufen, Sprinten - Anpassung im Code möglich -> Unterm [Header("FOV Settings")] einfach die Zahlenwerte ändern

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -14,15 +14,24 @@ public class Movement : MonoBehaviour
 
 
     [Header("Mouse Look")]
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 100f; // sens
     public Transform cameraTransform;    // char cam
     private float xRotation = 0f;        // camera pitch
 
+    [Header("FOV Settings")]
+    private float normalFOV = 60f;        // standard fov
+    private float sprintFOV = 67f;        // FOV sprinten
+    private float crouchFOV = 53f;      // FOV crouchen
+    private float fovLerpSpeed = 5f;      // FOV Anpassungsgeschwindigkeit
+
     private CharacterController controller;
+    private Camera cam;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        cam = cameraTransform.GetComponent<Camera>();
+
 
         if (cameraTransform == null)
             cameraTransform = Camera.main.transform;
@@ -39,40 +48,42 @@ public class Movement : MonoBehaviour
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //cam drehen
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // cam drehen
 
-        transform.Rotate(Vector3.up * mouseX); //player drehen
+        transform.Rotate(Vector3.up * mouseX); // player drehen
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 rawMove = transform.right * x + transform.forward * z; //bewegen in cam richtung
+        Vector3 rawMove = transform.right * x + transform.forward * z; // bewegen in cam richtung
         if (rawMove.sqrMagnitude > 1f)
-            rawMove = rawMove.normalized; //diagonal nicht schneller
+            rawMove = rawMove.normalized; // diagonal nicht schneller
 
-        bool shiftPressed = Input.GetKey(KeyCode.LeftShift); //shift gedrückt?
-        bool strgPressed = Input.GetKey(KeyCode.LeftControl); //strg gedrückt?
+        bool shiftPressed = Input.GetKey(KeyCode.LeftShift); // shift gedrückt?
+        bool strgPressed = Input.GetKey(KeyCode.LeftControl); // strg gedrückt?
 
-        float targetSpeed;
+        float targetSpeed; // einmal definieren zum Benutzen
+        float targetFOV;
+
         if (shiftPressed && !strgPressed)
         {
             targetSpeed = sprintSpeed;
+            targetFOV = sprintFOV;
         }
         else if (strgPressed && !shiftPressed)
         {
             targetSpeed = crouchSpeed;
+            targetFOV = crouchFOV;
         }
         else
         {
             targetSpeed = walkSpeed;
+            targetFOV = normalFOV;
         }
 
-        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * 10f); //speed unterschiede smoothen
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * 10f); // Speedänderung smoothen
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, Time.deltaTime * fovLerpSpeed); // FOV änderung smoothen
 
-        controller.Move(rawMove * currentSpeed * Time.deltaTime); //char endlich bewegen
+        controller.Move(rawMove * currentSpeed * Time.deltaTime); // char endlich bewegen
     }
 }
-
-
-
-
